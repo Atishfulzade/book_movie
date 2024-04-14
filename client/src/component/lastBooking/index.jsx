c459d2be569d2ff3ea13e75f9f5453c5955feedc
Atishfulzade <atishfulzade1234@gmail.com>
2024-04-14 12:37:43 +0530

    fied

--- client/src/component/lastBooking/index.jsx
+++ client/src/component/lastBooking/index.jsx
@@ -7,7 +7,9 @@ const LastSeatBooking = ({ isLoading, infoSubmitted }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await axios.get("http://localhost:3000/api/booking");
+        const result = await axios.get(
+          "https://book-movie-api.vercel.app/api/booking"
+        );
         // Extract the last booking from the response data array
         const lastBooking = result.data[result.data.length - 1];
         setLastBooking(lastBooking);
