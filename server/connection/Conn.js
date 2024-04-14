6eb41a6243885b8872f385b57a5e6f11e0009c3b
Atishfulzade <atishfulzade1234@gmail.com>
2024-04-14 13:02:25 +0530

    env file added

--- server/connection/Conn.js
+++ server/connection/Conn.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 async function connect() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://atishfulzade1234:KMx0rmWbcR2jkE6J@api-movie.mojcktp.mongodb.net/?retryWrites=true&w=majority&appName=api-movie",
-      {
-        dbName: "movie-booking", // Specify your database name
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "movie-booking", // Specify your database name
+    });
     console.log("Database connection established");
   } catch (error) {
     console.error("Error connecting to database:", error);
